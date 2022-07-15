e68fd6f01952ec5c3052c87f126b555dede842da
ErenDexter <2002021@che.buet.ac.bd>
2022-07-15 11:43:02 +0600

    testing

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ const app = express();
 
 app.get("/", (req, res) => {
   axios
-    .get("https://jdwel.com/today/", {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        Accept: "application/json",
-        "User-Agent":
-          "Mozilla/5.0 (Linux; U; Android 4.1.1; en-gb; Build/KLP) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30",
-      },
-    })
+    .get(
+      "https://jdwel.com/today/"
+      // ,{
+      //   headers: {
+      //     "Access-Control-Allow-Origin": "*",
+      //     Accept: "application/json",
+      //     "User-Agent":
+      //       "Mozilla/5.0 (Linux; U; Android 4.1.1; en-gb; Build/KLP) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30",
+      //   },
+      // }
+    )
     .then(function (data) {
       const $ = cheerio.load(data.data);
 
@@ -21,8 +24,8 @@ app.get("/", (req, res) => {
       $(".matches_datepicker").remove();
 
       const content = $(".matches_frame");
-      console.log(content.html());
-      res.send("success");
+      //console.log(content.html());
+      res.send(content.html());
     })
     .catch((error) => {
       console.log(error);
