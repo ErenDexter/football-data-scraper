4f30728018b6ce56e7ec9b86a3a2b06e9fb17ded
ErenDexter <2002021@che.buet.ac.bd>
2022-07-16 02:59:49 +0600

    done

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
     .then(function (data) {
       const $ = cheerio.load(data.data);
 
-      //$(".menu-section.main-nav.header-nav").remove();
+      $(".menu-section.main-nav.header-nav").remove();
       $(".widget_albatrteblist.container-wrapper").remove();
       $(".widget_categories.Alba-widget-categories.container-wrapper").remove();
       $(".sidebar.is-sticky").remove();
@@ -28,6 +28,12 @@ app.get("/", (req, res) => {
       $("#footer").remove();
       $(".grid-container").remove();
       $("#fixedban > div > div:nth-child(4)").remove();
+      $("#Today > div.albaflex").wrapAll(
+        '<div style="pointer-events: none;"></div>'
+      );
+      $("#Tomorrow > div.albaflex").wrapAll(
+        '<div style="pointer-events: none;"></div>'
+      );
       //$(".matches_datepicker").remove();
 
       //const content = $(".matches_frame");
