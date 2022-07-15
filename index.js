a9b0336fcd15f4ae19553e8473e69d8139c65ab6
ErenDexter <2002021@che.buet.ac.bd>
2022-07-15 12:00:36 +0600

    testing

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
   axios
-    .get(
-      "https://jdwel.com/today/"
-      // ,{
-      //   headers: {
-      //     "Access-Control-Allow-Origin": "*",
-      //     Accept: "application/json",
-      //     "User-Agent":
-      //       "Mozilla/5.0 (Linux; U; Android 4.1.1; en-gb; Build/KLP) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30",
-      //   },
-      // }
-    )
+    .get("https://jdwel.com/today/", {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        Accept: "application/json",
+        "User-Agent":
+          "Mozilla/5.0 (Linux; U; Android 4.1.1; en-gb; Build/KLP) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30",
+      },
+    })
     .then(function (data) {
       const $ = cheerio.load(data.data);
 
