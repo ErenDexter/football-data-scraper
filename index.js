ad9ab08bde9a4f3e17898161f7e5cc3922c2ec51
ErenDexter <2002021@che.buet.ac.bd>
2022-07-15 11:29:05 +0600

    testing

--- index.js
+++ index.js
@@ -5,29 +5,29 @@ const cheerio = require("cheerio");
 const app = express();
 
 app.get("/getdata", (req, res) => {
-  axios
-    .get("https://jdwel.com/today/", {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        Accept: "application/json",
-        "User-Agent":
-          "Mozilla/5.0 (Linux; U; Android 4.1.1; en-gb; Build/KLP) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30",
-      },
-    })
-    .then(function (data) {
-      const $ = cheerio.load(data.data);
+  // axios
+  //   .get("https://jdwel.com/today/", {
+  //     headers: {
+  //       "Access-Control-Allow-Origin": "*",
+  //       Accept: "application/json",
+  //       "User-Agent":
+  //         "Mozilla/5.0 (Linux; U; Android 4.1.1; en-gb; Build/KLP) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30",
+  //     },
+  //   })
+  //   .then(function (data) {
+  //     const $ = cheerio.load(data.data);
 
-      $(".submenu.row.justify-content-end").remove();
-      $(".matches_datepicker").remove();
+  //     $(".submenu.row.justify-content-end").remove();
+  //     $(".matches_datepicker").remove();
 
-      const content = $(".matches_frame");
-      //console.log(content.html());
-      res.send(content.html());
-    })
-    .catch((error) => {
-      console.log(error);
-      res.send(error);
-    });
+  //     const content = $(".matches_frame");
+  //console.log(content.html());
+  res.send(content.html());
+  // })
+  // .catch((error) => {
+  //   console.log(error);
+  //   res.send(error);
+  // });
 });
 
 const PORT = 5000;
